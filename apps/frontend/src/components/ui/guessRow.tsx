28fa9ace0713f668307e438528a12d5fe776e332
Jhonatan Brito <jhonatanjesusbrito@gmail.com>
2026-09-14 16:43:00 -0400

    feat: Add bigger arrow and yellow when close to Birth Year box

--- apps/frontend/src/components/ui/guessRow.tsx
+++ apps/frontend/src/components/ui/guessRow.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown, ArrowUp } from 'lucide-react';
+import { ArrowBigDown, ArrowBigUp } from 'lucide-react';
 import type { Comparison } from '@venedle/shared/src/types/guesses';
 import { cn } from '@/lib/utils';
 import { shortValue } from '@/lib/categories';
@@ -32,20 +32,38 @@ export function GuessRow({
           title={col.guessedValue}
           className={cn(
             `cell relative flex flex-col items-center justify-center border-2 px-2 border-black shadow-lg/20 text-shadow-lg/50 gap-0.5 rounded-xl aspect-9/10`,
-            col.match
-              ? 'bg-correct text-white shadow-sm'
-              : 'bg-miss text-white',
+            {
+              'bg-correct text-white ': col.match,
+              'bg-miss text-white': !col.match,
+              'bg-close-guess text-white':
+                col.category === 'Birth Year' &&
+                !col.match &&
+                Math.abs(
+                  parseInt(col.targetValue) - parseInt(col.guessedValue),
+                ) < 6,
+            },
           )}
         >
-          <span className="w-full text-center text-[12px] font-bold leading-tight">
+          <span className="w-full text-center text-[12px] font-bold leading-tight z-10">
             {shortValue(col.guessedValue)}
           </span>
           {!col.match && col.direction && (
-            <span className="text-[9px] leading-none" aria-label="pista">
+            <span
+              className="text-[9px] leading-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              aria-label="pista"
+            >
               {col.direction === 'higher' ? (
-                <ArrowUp className="size-3" />
+                <ArrowBigUp
+                  fill="#222"
+                  strokeWidth={0}
+                  className="size-20 opacity-50"
+                />
               ) : (
-                <ArrowDown className="size-3" />
+                <ArrowBigDown
+                  fill="#222"
+                  strokeWidth={0}
+                  className="size-20 opacity-50"
+                />
               )}
             </span>
           )}
